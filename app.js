d861d142d29af9545189fa5c65a6ce4894bb8bf7
Nimit <nimitgajera3@gmail.com>
2022-09-16 15:11:52 +0530

    15/9/2022 commit

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ const dotenv= require('dotenv/config')
 const morgan= require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const { router } = require('json-server');
 
 app.use(morgan("tiny"));
 app.use(cors());
+app.options("*",cors());
+
+
+
+app.use(express.json());
 
 mongoose.connect(process.env.CONNECTION_URL)
+
 .then(()=>{
     console.log("Database connection is ready...")
 }).catch((err)=>{
     console.log(err);
 })
 
+const productRoutes = require("./routes/products");
+app.use(`/products`,productRoutes)
 
-
-app.get('/',(req,res)=> res.send('hello'))
-app.post('/post',(req,res)=>{
-    res.send('started')
-})
 
 app.listen(process.env.PORT,()=> console.log(`app listening onn port ${process.env.PORT}`))
